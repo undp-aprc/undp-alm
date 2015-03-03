a448e236cfeba236fafb8490cb8d98b493063316
Matthew Clark <matthew.clark@undp.org>
2015-03-03 13:31:06 +1100

    Improved presentation on mobile devices

--- modules/custom/theme_modules/cca_theme_blocks/templates/cca-media-menu.tpl.php
+++ modules/custom/theme_modules/cca_theme_blocks/templates/cca-media-menu.tpl.php
@@ -1,15 +1,15 @@
 <?php $theme_path = $variables['elements']['variables']['theme_path']; ?>
-<h4>Media Center</h4>
+<h4 class="hidden-xs">Media Center</h4>
 <ul class="resource-menu-list inline-menu">
         <li>
             <a href="/news">
-                <img src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-news.svg" />
+                <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-news.svg" />
                 <h5>News</h5>
             </a>
         </li>
         <li>
             <a href="/videos">
-                <img src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-video.svg" />
+                <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-video.svg" />
                 <h5>Videos</h5>
             </a>
         </li>
--- modules/custom/theme_modules/cca_theme_blocks/templates/cca-signature-programs.tpl.php
+++ modules/custom/theme_modules/cca_theme_blocks/templates/cca-signature-programs.tpl.php
@@ -1,9 +1,9 @@
-<h4>Signature Programs</h4>
+<h4 class="hidden-xs">Signature Programs</h4>
 <ul class="signature-programs-list inline-menu">
     <?php foreach($variables['elements']['#items'] as $item): ?>
         <li>
             <a href="/taxonomy/term/<?php print $item['tid'] ?>">
-                <?php print(render($item[img])); ?>
+                <span class="hidden-xs"><?php print(render($item['img'])); ?></span>
                 <h5><?php print $item['title']; ?></h5>
             </a>
         </li>
