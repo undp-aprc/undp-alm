c33fc7587f883dc18ab5389c587df22deb546f78
Matthew Clark <matthew.clark@undp.org>
2015-03-11 16:50:36 +1100

    Added conditional 'see all' buttons to home page section blocks'

--- modules/custom/theme_modules/cca_theme_blocks/templates/cca-section-container.tpl.php
+++ modules/custom/theme_modules/cca_theme_blocks/templates/cca-section-container.tpl.php
@@ -1,5 +1,10 @@
 <?php $elements = $variables['elements']; ?>
 <div class="row section-content hidden-xs" id="<?php print $elements['#dom_variables']['section-container-id']; ?>">
-    <h1><?php print $elements['#dom_variables']['section-container-heading']; ?></h1>
+    <h1>
+        <?php print $elements['#dom_variables']['section-container-heading']; ?>
+        <?php if($elements['#dom_variables']['more-content']): ?>
+            <a class="btn btn-default" href="<?php print($elements['#dom_variables']['more-content']); ?>">See All ></a>
+        <?php endif; ?>
+    </h1>
     <?php print $elements['#children']; ?>
 </div> <!-- #section-what-we-do -->
