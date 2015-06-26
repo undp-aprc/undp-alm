fd8748f74b1b7732a3f3534d16efe26ef2ed599f
Matt Clark <matthew.clark@undp.org>
2015-06-26 12:50:20 +1000

    Added description text to signature programme landing page

--- themes/custom/cca/templates/taxonomy-term--signature-programmes--signature-programme-header.tpl.php
+++ themes/custom/cca/templates/taxonomy-term--signature-programmes--signature-programme-header.tpl.php
@@ -4,6 +4,14 @@
         <?php if ($content['field_program_preview_img']): ?>
             <?php print render($content['field_program_preview_img']); ?>
         <?php endif; ?>
-        <h1><?php print ($term_name); ?></h1>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1 class="panel-title"><?php print ($term_name); ?></h1>
+            </div>
+            <div class="panel-body">
+                <?php print(render($content['description'])); ?>
+            </div>
+        </div>
     </div>
 </div>
+<h2 class="pane-title">Supported Programmes</h2>
