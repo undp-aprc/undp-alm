4fa4d2d64a25524f8c124eb8091d1507584e3c2e
Matt Clark <matthew.clark@undp.org>
2015-07-04 10:13:47 +1000

    Updated signature programme header style

--- themes/custom/cca/templates/taxonomy-term--signature-programmes--signature-programme-header.tpl.php
+++ themes/custom/cca/templates/taxonomy-term--signature-programmes--signature-programme-header.tpl.php
@@ -1,14 +1,14 @@
 <div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?> view-mode-signature-programme-header">
     <div class="term-page-header">
         <h2>Signature Programme <span class="badge">#<?php print($pos); ?></span></h2>
-        <?php if ($content['field_program_preview_img']): ?>
-            <div class="left">
-                <?php print(render($content['field_program_preview_img'])); ?>
-            </div>
-        <?php endif; ?>
-        <div class="right">
-            <h1><?php print($name); ?></h1>
-            <?php print(render($content['description'])); ?>
+            <div class="image-container">
+                <?php if ($content['field_program_preview_img']): ?>
+                    <?php print(render($content['field_program_preview_img'])); ?>
+                <?php endif; ?>
+                <div class="overlay">
+                    <h1><?php print($name); ?></h1>
+                    <?php print(render($content['field_summary'])); ?>
+                </div>
         </div>
     </div>
 </div>
