4f35859941d58a1d346375cb6797265a0d557b2c
Matthew Clark <matthew.clark@undp.org>
2015-11-26 14:46:05 +0700

    Updated project masthead image

--- themes/custom/cca/templates/node--project--masthead.tpl.php
+++ themes/custom/cca/templates/node--project--masthead.tpl.php
@@ -81,7 +81,7 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-    <div class="content"<?php print $content_attributes; ?>>
+    <div class="content project-masthead"<?php print $content_attributes; ?>>
         <?php
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
@@ -91,9 +91,4 @@
 
         <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
     </div>
-
-    <?php print render($content['links']); ?>
-
-    <?php print render($content['comments']); ?>
-
 </div>
