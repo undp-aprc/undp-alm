c7707467caa60dab52d4ba1f91adc76c64fb4785
Matthew Clark <matthew.clark@undp.org>
2015-12-04 10:54:24 +0700

    Updates to theme templates and CSS

--- themes/custom/cca/templates/node--video--video-preview-no-titles.tpl.php
+++ themes/custom/cca/templates/node--video--video-preview-no-titles.tpl.php
@@ -83,6 +83,5 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <div class="content"<?php print $content_attributes; ?>>
         <?php print render($content); ?>
-        <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
     </div>
 </div>
